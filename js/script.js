6edd5aba7dfc619943f3a5c5d6637b429074242d
OLEKSANDR VEREMIIENKO <120383111+SkyKeyUA@users.noreply.github.com>
2026-03-05 10:21:51 -0700

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -121,3 +121,24 @@ if (document.querySelector('.community__swiper-community')) {
     });
   })();
 }
+const options = {
+  root: null,
+  rootMargin: '0px 0px 0px 0px',
+  threshold: 0.3,
+};
+const callback = (entries, observer) => {
+  entries.forEach((entry) => {
+    const currentElement = entry.target;
+    if (entry.isIntersecting) {
+      currentElement.classList.add('animate');
+    } else {
+      currentElement.classList.remove('animate');
+    }
+  });
+};
+const observer = new IntersectionObserver(callback, options);
+
+const animElements = document.querySelectorAll('[class*="--anim"]');
+animElements.forEach((animElement) => {
+  observer.observe(animElement);
+});
